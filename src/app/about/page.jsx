8b7cdb92655286f15a22815303e9dcbf37df2339
Hamza Qadeer <hamza@qadeer.com>
2024-06-25 02:42:27 -0700

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-3">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie website! We are a team of passionate movie
+        enthusiasts who have come together to create a non-stop destination for
+        all your movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news, reviewws,
+        and trailors. Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of cinema. You can search for movies by title, director,
+        actor, genre, or release date, making it easy to find the perfect movie
+        for any occassion.
+      </p>
+    </div>
+  );
+}
